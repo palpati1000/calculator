25c9b2e764b727a1a7db27165ac6e1a7dbcae1ec
palpati1000 <pravinalpati1000@Gmail.com>
2025-03-16 14:50:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def calculator():
     return render_template('calculator.html', result=result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=80)
